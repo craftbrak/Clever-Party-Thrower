c82d4e5289d13c973552a6c8a178fb4fd610b500
craftbrak <louisdewilde2001@gmail.com>
2023-05-04 18:36:30 +0200

    back-end: adding CROS

--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -9,6 +9,12 @@ async function bootstrap() {
   const port = configService.get<string>("PORT");
   const logger = new Logger("MAIN");
   app.useGlobalPipes(new ValidationPipe());
+  // Enable CORS for your application
+  app.enableCors({
+    origin: "*", // Allow any origin, or you can specify a specific domain, e.g., 'http://localhost:4200'
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+    allowedHeaders: "Content-Type, Accept, Authorization",
+  });
   await app.listen(port, "0.0.0.0");
   logger.log(`listening at 0.0.0.0 on on port:${port}`);
 }
